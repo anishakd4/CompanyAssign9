9e8f9fe73a3ef23e9476c7684b1d1653f979535a
Anish <anishakd4@gmail.com>
2020-02-12 11:53:43 +0530

    init

--- app/src/main/java/com/developer/anishakd4/ymedialabstest/fragments/OverviewFragment.kt
+++ app/src/main/java/com/developer/anishakd4/ymedialabstest/fragments/OverviewFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.developer.anishakd4.ymedialabstest.adapters.RecyclerviewAdapter
 import com.developer.anishakd4.ymedialabstest.databinding.FragmentOverviewBinding
 
 class OverviewFragment : Fragment() {
@@ -19,6 +20,8 @@ class OverviewFragment : Fragment() {
         val binding = FragmentOverviewBinding.inflate(inflater)
         binding.setLifecycleOwner(this)
         binding.viewModel = viewModel
+
+        binding.list.adapter = RecyclerviewAdapter()
 
         return binding.root
     }
